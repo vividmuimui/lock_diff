9ad312902b537f630ff48fad0d2bb7baaf9ba015
Rastam Hadi <rastam.hadi@gmail.com>
2020-10-01 14:41:35 +0900

    Fix broken LockDiff::Github::RepositoryNameDetector test
    https://travis-ci.org/github/vividmuimui/lock_diff/jobs/581970224
    
    LockDiff::Github::RepositoryNameDetector is expected to eq "rrg/rr"
    Failure/Error: expect(RepositoryNameDetector.new("https://github.com/rr/rr/foo#readme").call).to eq 'rrg/rr'
    
      expected: "rrg/rr"
          got: "rr/rr"
    
      (compared using ==)

--- spec/lock_diff/github/repository_name_detector_spec.rb
+++ spec/lock_diff/github/repository_name_detector_spec.rb
@@ -5,7 +5,7 @@ module LockDiff::Github
     specify do
       expect(RepositoryNameDetector.new("https://github.com/rr/rr").call).to eq 'rr/rr'
       expect(RepositoryNameDetector.new("https://github.com/rr/rr/foo/bar/baz").call).to eq 'rr/rr'
-      expect(RepositoryNameDetector.new("https://github.com/rr/rr/foo#readme").call).to eq 'rrg/rr'
+      expect(RepositoryNameDetector.new("https://github.com/rr/rr/foo#readme").call).to eq 'rr/rr'
       expect(RepositoryNameDetector.new('https://rubygems.org/gems/rr').call).to eq nil
       expect(RepositoryNameDetector.new(nil).call).to eq nil
     end
